37bd71a355c579f84ef30bfe6fa11514838d5c0c
OBrian16 <briwell16@gmail.com>
2019-05-21 20:21:58 +0300

    signup form with semantic

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
-import { registerUser } from '../actions/authentification'
+import { registerUser } from '../actions/authentification';
+import classnames from 'classnames';
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
 class Register extends Component {
 
@@ -34,79 +36,105 @@ class Register extends Component {
             password_confirm: this.state.password_confirm
         }
         this.props.registerUser(user, this.props.history);
-        //console.log(user);
     }
 
     componentWillReceiveProps(nextProps) {
-        if(nextProps.errors) {
+        if (nextProps.errors) {
             this.setState({
                 errors: nextProps.errors
-            })
+            });
         }
     }
 
     render() {
-        return(
-        <div className="container" style={{ marginTop: '50px', width: '700px'}}>
-            <h2 style={{marginBottom: '40px'}}>Register</h2>
-            <form onSubmit={ this.handleSubmit }>
-                <div className="form-group">
-                    <input
-                    type="text"
-                    placeholder="Name"
-                    className="form-control"
-                    name="name"
-                    onChange={ this.handleInputChange }
-                    value={ this.state.name }
-                    />
-                </div>
-                <div className="form-group">
-                    <input
-                    type="email"
-                    placeholder="Email"
-                    className="form-control"
-                    name="email"
-                    onChange={ this.handleInputChange }
-                    value={ this.state.email }
-                    />
-                </div>
-                <div className="form-group">
-                    <input
-                    type="password"
-                    placeholder="Password"
-                    className="form-control"
-                    name="password"
-                    onChange={ this.handleInputChange }
-                    value={ this.state.password }
-                    />
-                </div>
-                <div className="form-group">
-                    <input
-                    type="password"
-                    placeholder="Confirm Password"
-                    className="form-control"
-                    name="password_confirm"
-                    onChange={ this.handleInputChange }
-                    value={ this.state.password_confirm }
-                    />
-                </div>
-                <div className="form-group">
-                    <button type="submit" className="btn btn-primary">
-                        Register
-                    </button>
-                </div>
-            </form>
-        </div>
+        const { errors } = this.state;
+        return (
+            <div className="container"
+                style={{ marginTop: '50px', width: '700px' }}>
+                <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+                    <Grid.Column style={{ maxWidth: 450 }}>
+
+                        <Header as='h2' color='teal' textAlign='center'>
+                            <Image src='/logo.png' /> Create Your Account
+                    </Header>
+
+                        <Form onSubmit={this.handleSubmit} size='large'>
+                            <Segment stacked>
+                                <Form.Input
+                                    icon=''
+                                    iconPosition='left'
+                                    type="text"
+                                    placeholder="Name"
+                                    name="name"
+                                    onChange={this.handleInputChange}
+                                    value={this.state.name}
+                                />
+
+                                {errors.name && (<div className="invalid-feedback">{errors.name}</div>)}
+
+                                <Form.Input
+                                    fluid
+                                    icon='user'
+                                    iconPosition='left'
+                                    type="email"
+                                    placeholder="Email"
+                                    name="email"
+                                    onChange={this.handleInputChange}
+                                    value={this.state.email}
+                                />
+
+                                {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
+
+                                <Form.Input
+                                    fluid
+                                    icon='lock'
+                                    iconPosition='left'
+                                    type="password"
+                                    placeholder="Password"
+                                    name="password"
+                                    onChange={this.handleInputChange}
+                                    value={this.state.password}
+                                />
+
+                                {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
+
+                                <Form.Input
+                                    fluid
+                                    icon='lock'
+                                    iconPosition='left'
+                                    type="password"
+                                    placeholder="Confirm Password"
+                                    name="password_confirm"
+                                    onChange={this.handleInputChange}
+                                    value={this.state.password_confirm}
+                                />
+
+                                {errors.password_confirm && (<div className="invalid-feedback">{errors.password_confirm}</div>)}
+
+                                <Button color='teal' fluid size='large'>
+                                    Register
+                                </Button>
+
+                            </Segment>
+                        </Form>
+
+                        <Message>
+                            CCCCCCCCCCC
+                        </Message>
+
+                    </Grid.Column>
+                </Grid>
+            </div >
         )
     }
 }
 
 Register.propTypes = {
-    registerUser: PropTypes.func.isRequired
+    registerUser: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
     errors: state.errors
 });
 
-export default connect(mapStateToProps,{ registerUser })(withRouter (Register))
+export default connect(mapStateToProps, { registerUser })(withRouter(Register))
