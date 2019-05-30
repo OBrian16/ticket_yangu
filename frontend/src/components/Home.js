b9bd54b3a3a342b626cdcfd6b902bf1856911e68
OBrian16 <briwell16@gmail.com>
2019-05-30 23:50:38 +0300

    Add carausel to homepage

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import Carousel from 'semantic-ui-carousel-react';
+import { Button, Image, Container, Segment, Header, Message } from 'semantic-ui-react'
+
+import "pure-react-carousel/dist/react-carousel.es.css";
+
+//import CardCarousel from '../carousel/card';
+import ImageCarousel from '../carousel/image'
 
 export default class Home extends Component {
     render() {
-        return(
-            <div>
-                Home Component
-            </div>
-        );
+
+        return (
+            <Container style={{ margin: 20 }}>
+
+                <Segment attached="bottom">
+                    <ImageCarousel />
+                </Segment>
+
+                <Message info>
+                    xxxxxx
+                </Message>
+            </Container>
+        )
     }
 }
