184d32fa6f24502ce33c0aedd521ba35a018248a
OBrian16 <briwell16@gmail.com>
2019-07-03 15:38:58 +0300

    Edit carousel

--- frontend/src/carousel/image.js
+++ frontend/src/carousel/image.js
@@ -1,30 +1,64 @@
-import { CarouselProvider, Image, Slide, Slider } from "pure-react-carousel";
 import React from "react";
-import { Divider } from "semantic-ui-react";
+import { MDBCarousel, MDBCarouselCaption, MDBCarouselInner, MDBCarouselItem, MDBView, MDBMask, MDBContainer } from
+  "mdbreact";
 
-import CustomDotGroup from './customDotSlide';
+const CarouselPage = () => {
+  return (
+    <MDBContainer>
+      <MDBCarousel
+        activeItem={1}
+        length={3}
+        showControls={true}
+        showIndicators={true}
+        className="z-depth-1"
+      >
+        <MDBCarouselInner>
+          <MDBCarouselItem itemId="1">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg"
+                alt="First slide"
+              />
+              <MDBMask overlay="black-light" />
+            </MDBView>
+            <MDBCarouselCaption>
+              <h3 className="h3-responsive">Light mask</h3>
+              <p>First text</p>
+            </MDBCarouselCaption>
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId="2">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).jpg"
+                alt="Second slide"
+              />
+              <MDBMask overlay="black-strong" />
+            </MDBView>
+            <MDBCarouselCaption>
+              <h3 className="h3-responsive">Strong mask</h3>
+              <p>Second text</p>
+            </MDBCarouselCaption>
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId="3">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src="https://mdbootstrap.com/img/Photos/Slides/img%20(9).jpg"
+                alt="Third slide"
+              />
+              <MDBMask overlay="black-slight" />
+            </MDBView>
+            <MDBCarouselCaption>
+              <h3 className="h3-responsive">Slight Mast</h3>
+              <p>Third text</p>
+            </MDBCarouselCaption>
+          </MDBCarouselItem>
+        </MDBCarouselInner>
+      </MDBCarousel>
+    </MDBContainer>
+  );
+}
 
-const ImageCarousel = () => (
-  <CarouselProvider
-    naturalSlideWidth={1}
-    naturalSlideHeight={1}
-    totalSlides={3}
-  >
-    <Slider>
-      <Slide tag="a" index={0}>
-        <Image src="/0" />
-      </Slide>
-      <Slide tag="a" index={1}>
-        <Image src="/1" />
-      </Slide>
-      <Slide tag="a" index={2}>
-        <Image src="/2" />
-      </Slide>
-    </Slider>
-
-    <Divider />
-    <CustomDotGroup slides={3} />
-  </CarouselProvider>
-);
-
-export default ImageCarousel;
+export default CarouselPage;
