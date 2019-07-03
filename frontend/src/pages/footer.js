2033763d74392e928af45bcd5c594521449642f8
OBrian16 <briwell16@gmail.com>
2019-07-03 15:34:46 +0300

    Add footer

---
+++ frontend/src/pages/footer.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import {
+    Container,
+    Divider,
+    Dropdown,
+    Grid,
+    Header,
+    Image,
+    List,
+    Menu,
+    Segment,
+    Responsive
+} from 'semantic-ui-react'
+import SocialLink from './social'
+
+// Heads up!
+// We using React Static to prerender our docs with server side rendering, this is a quite simple solution.
+// For more advanced usage please check Responsive docs under the "Usage" section.
+const getWidth = () => {
+    const isSSR = typeof window === 'undefined'
+  
+    return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth
+  }
+  
+  /* eslint-disable react/no-multi-comp */
+  /* Heads up! HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled components for
+   * such things.
+   */
+
+const Footer = () => (
+    <div>
+        <Segment inverted vertical style={{ minWidth:700, padding: '5em 0em' }}>
+            <Container textAlign='center'>
+                <Grid divided inverted stackable>
+                    <Grid.Column width={3}>
+                        <Header inverted as='h4' content='Group 1' />
+                        <List link inverted>
+                            <List.Item as='a'>Link One</List.Item>
+                            <List.Item as='a'>Link Two</List.Item>
+                            <List.Item as='a'>Link Three</List.Item>
+                            <List.Item as='a'>Link Four</List.Item>
+                        </List>
+                    </Grid.Column>
+                    <Grid.Column width={3}>
+                        <Header inverted as='h4' content='Group 2' />
+                        <List link inverted>
+                            <List.Item as='a'>Link One</List.Item>
+                            <List.Item as='a'>Link Two</List.Item>
+                            <List.Item as='a'>Link Three</List.Item>
+                            <List.Item as='a'>Link Four</List.Item>
+                        </List>
+                    </Grid.Column>
+                    <Grid.Column width={3}>
+                        <Header inverted as='h4' content='Group 3' />
+                        <List link inverted>
+                            <List.Item as='a'>Link One</List.Item>
+                            <List.Item as='a'>Link Two</List.Item>
+                            <List.Item as='a'>Link Three</List.Item>
+                            <List.Item as='a'>Link Four</List.Item>
+                        </List>
+                    </Grid.Column>
+                    <Grid.Column width={7}>
+                        <Header inverted as='h4' content='Connect with Us' />
+                        <p>
+                            <SocialLink/>
+                        </p>
+                    </Grid.Column>
+                </Grid>
+
+                <Divider inverted section />
+                Copyright © Ticketyangu 2019
+            </Container>
+        </Segment>
+    </div>
+)
+
+export default Footer
