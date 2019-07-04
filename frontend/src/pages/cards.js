928355b17d5de8d6c7c2bdab9534ed65f58b6684
OBrian16 <briwell16@gmail.com>
2019-07-04 23:52:40 +0300

    Add events

--- frontend/src/pages/cards.js
+++ frontend/src/pages/cards.js
@@ -1,19 +1,16 @@
 import React from 'react'
-import { Card, Icon, Image, CardGroup } from 'semantic-ui-react'
+import { Card, Icon, Image, CardGroup, Button, Divider, Segment } from 'semantic-ui-react'
 
 const EventCard = () => (
     <Card.Group itemsPerRow={4}>
 
         <Card>
-        <Image src={require('../images/eventImages/dfd.jpg')}
-            
+            <Image src={require('../images/eventImages/dfd.jpg')}
             wrapped ui={false} />
             <Card.Content>
-                <Card.Header>Event 1</Card.Header>
-                <Card.Meta><b><code>26TH JULY</code></b></Card.Meta>
-                <Card.Description>
-                    Event Description
-                </Card.Description>
+                <Card.Header>Design For Designers</Card.Header>
+                <Card.Meta><b><code>FRI 26TH JULY</code></b></Card.Meta>
+
             </Card.Content>
         </Card>
 
@@ -21,35 +18,28 @@ const EventCard = () => (
             <Image src={require('../images/eventImages/ols.jpg')}
                 wrapped ui={false} />
             <Card.Content>
-                <Card.Header>Event 2</Card.Header>
-                <Card.Meta>26TH JULY</Card.Meta>
-                <Card.Description>
-                    Event Description
-                </Card.Description>
+                <Card.Header>Old School Mondays</Card.Header>
+                <Card.Meta><b><code>MON 30TH JULY</code></b></Card.Meta>
+
             </Card.Content>
         </Card>
 
         <Card>
-        <Image src={require('../images/eventImages/etana.jpg')}
-            wrapped ui={false} />
+            <Image src={require('../images/eventImages/etana.jpg')}
+                wrapped ui={false} />
             <Card.Content>
-                <Card.Header>Event 3</Card.Header>
-                <Card.Meta>26TH JULY</Card.Meta>
-                <Card.Description>
-                    Event Description
-                </Card.Description>
+                <Card.Header>Etana Live</Card.Header>
+                <Card.Meta><b><code>SAT 21ST AUGUST</code></b></Card.Meta>
+ 
             </Card.Content>
         </Card>
 
         <Card>
-        <Image src={require('../images/eventImages/mkms.jpg')}
-            wrapped ui={false} />
+            <Image src={require('../images/eventImages/mkms.jpg')}
+                wrapped ui={false} />
             <Card.Content>
-                <Card.Header>Event 4</Card.Header>
-                <Card.Meta>26TH JULY</Card.Meta>
-                <Card.Description>
-                    Event Description
-                </Card.Description>
+                <Card.Header>My Kenya My Story</Card.Header>
+                <Card.Meta><b><code>sun 28TH AUGUST</code></b></Card.Meta>
             </Card.Content>
         </Card>
 
