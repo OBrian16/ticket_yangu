55d91caef0b350292528d1ae62ef00c2354d08f3
OBrian16 <briwell16@gmail.com>
2019-07-04 13:11:45 +0300

    Add Responsiveness to homepage

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,59 +1,74 @@
+import PropTypes from 'prop-types'
 import React, { Component } from 'react'
-import Carousel from 'semantic-ui-carousel-react';
-import { Button, Image, Icon, Container, Segment, Header, Message } from 'semantic-ui-react'
+import { Button, Container, Divider, Grid, Header, Icon, Image, List, Menu, Responsive, Segment, Sidebar, Visibility } from 'semantic-ui-react'
 
-import "pure-react-carousel/dist/react-carousel.es.css";
-
-//import CardCarousel from '../carousel/card';
-import ImageCarousel from '../carousel/image'
-import EventCards from '../pages/cards'
 import Footer from '../pages/footer'
+import CarouselPage from '../carousel/image'
 
-export default class Home extends Component {
+// Heads up!
+// We using React Static to prerender our docs with server side rendering, this is a quite simple solution.
+// For more advanced usage please check Responsive docs under the "Usage" section.
+const getWidth = () => {
+    const isSSR = typeof window === 'undefined'
+
+    return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth
+}
+
+
+class DesktopContainer extends Component {
+    state = {}
+
     render() {
+        const { children } = this.props
+        const { fixed } = this.state
 
         return (
-            <Container>
-                <style>{`
-              html, body {
-                background-color: #F2F3F4 !important;
-              }
-              p {
-                align-content: center;
-                color: #fff;
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                min-width: 20em;
-              }
-              p > span {
-                opacity: 0.4;
-                text-align: center;
-              }
-              }
-            `}
-                </style>
-                <Segment attached="">
-                    <ImageCarousel />
+            <Responsive getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth}>
+                <Segment
+                    //inverted
+                    textAlign='center'
+                    style={{ minHeight: 300, padding: '2em 0em' }}
+                    vertical
+                >
+                    <CarouselPage />
                 </Segment>
 
-                <Segment clearing textAlign='center'>
-                    <b>Upcoming Events</b>
-
-                    <Button color='red' floated='right'>
-                        <Icon name='calendar alternate outline' /> More Events
-                    </Button>
-                </Segment>
-
-                <Segment attached="bottom">
-                    <EventCards />
-                </Segment>
-
-                <Footer />
-
-            </Container>
-
-
+                {children}
+            </Responsive>
         )
     }
 }
+
+DesktopContainer.propTypes = {
+    children: PropTypes.node,
+}
+
+
+const ResponsiveContainer = ({ children }) => (
+    <div>
+        <DesktopContainer>{children}</DesktopContainer>
+    </div>
+)
+
+ResponsiveContainer.propTypes = {
+    children: PropTypes.node,
+}
+
+const HomepageLayout = () => (
+    <ResponsiveContainer>
+        <Segment style={{ padding: '8em 0em' }} vertical>
+
+        </Segment>
+        <Segment style={{ padding: '0em' }} vertical>
+            
+        </Segment>
+        <Segment style={{ padding: '8em 0em' }} vertical>
+            
+        </Segment>
+
+        <Segment inverted vertical style={{ padding: '0em 0em' }}>
+            <Footer />
+        </Segment>
+    </ResponsiveContainer>
+)
+export default HomepageLayout
