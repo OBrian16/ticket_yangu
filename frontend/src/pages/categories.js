8f6829ecd60f3ff0da425d52938e457a172abcf3
OBrian16 <briwell16@gmail.com>
2019-07-05 15:41:51 +0300

    add categort page

---
+++ frontend/src/pages/categories.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Card, Icon, Image, CardGroup, Button, Divider, Segment } from 'semantic-ui-react'
+// import { Link } from 'react-router-dom';
+
+
+const EventCategory = () => (
+    <Card.Group itemsPerRow={7}>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventCategory/festival.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Festivals</Card.Header>
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/ols.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Concerts</Card.Header>
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/etana.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Corporate</Card.Header>
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/mkms.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Sports</Card.Header>
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/dfd.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Club Nights</Card.Header>
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventCategory/festival.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Theatre & Comedy</Card.Header>
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/etana.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Dayouts</Card.Header>
+            </Card.Content>
+        </Card>
+
+    </Card.Group>
+)
+
+export default EventCategory
