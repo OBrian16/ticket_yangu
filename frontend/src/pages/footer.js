f0aeb08d33a7b064df00fabc930dada6b802849f
OBrian16 <briwell16@gmail.com>
2019-07-06 15:04:53 +0300

    Rename footer links

--- frontend/src/pages/footer.js
+++ frontend/src/pages/footer.js
@@ -15,45 +15,49 @@ import SocialLink from './social'
 
 const Footer = () => (
     <div>
-        <Segment inverted vertical style={{ minWidth:700, padding: '5em 0em' }}>
-            <Container textAlign='center'>
-                <Grid divided inverted stackable>
+        <Segment inverted vertical style={{ minWidth: 700, padding: '5em 0em' }}>
+            <Container >
+                <Grid divided inverted stackable textAlign='left'>
                     <Grid.Column width={3}>
-                        <Header inverted as='h4' content='Group 1' />
+                        <Header inverted as='h4' content='Services' />
                         <List link inverted>
-                            <List.Item as='a'>Link One</List.Item>
-                            <List.Item as='a'>Link Two</List.Item>
-                            <List.Item as='a'>Link Three</List.Item>
-                            <List.Item as='a'>Link Four</List.Item>
+                            <List.Item as='a'>Events</List.Item>
+                            <List.Item as='a'>Blogs</List.Item>
+                            <List.Item as='a'>Login</List.Item>
+                            <List.Item as='a'>Register</List.Item>
+                            <List.Item as='a'>Write a review</List.Item>
+
+
                         </List>
                     </Grid.Column>
                     <Grid.Column width={3}>
-                        <Header inverted as='h4' content='Group 2' />
+                        <Header inverted as='h4' content='Usage' />
                         <List link inverted>
-                            <List.Item as='a'>Link One</List.Item>
-                            <List.Item as='a'>Link Two</List.Item>
+                            <List.Item as='a'>Site Search</List.Item>
+                            <List.Item as='a'>Buy a Ticket</List.Item>
+                            <List.Item as='a'>Sell your Ticket</List.Item>
                             <List.Item as='a'>Link Three</List.Item>
-                            <List.Item as='a'>Link Four</List.Item>
+                            <List.Item as='a'>FAQ's</List.Item>
                         </List>
                     </Grid.Column>
                     <Grid.Column width={3}>
-                        <Header inverted as='h4' content='Group 3' />
+                        <Header inverted as='h4' content='Quicklinks' />
                         <List link inverted>
-                            <List.Item as='a'>Link One</List.Item>
-                            <List.Item as='a'>Link Two</List.Item>
-                            <List.Item as='a'>Link Three</List.Item>
-                            <List.Item as='a'>Link Four</List.Item>
+                            <List.Item as='a'>Pricing</List.Item>
+                            <List.Item as='a'>Support</List.Item>
+                            <List.Item as='a'>Private Policy</List.Item>
+                            <List.Item as='a'>Terms and Conditions </List.Item>
                         </List>
                     </Grid.Column>
                     <Grid.Column width={7}>
                         <Header inverted as='h4' content='Connect with Us' />
                         <p>
-                            <SocialLink/>
+                            <SocialLink />
                         </p>
                     </Grid.Column>
                 </Grid>
 
-                <Divider inverted section />
+                <Divider textAlign='center' inverted section />
                 Copyright © Ticketyangu 2019
             </Container>
         </Segment>
