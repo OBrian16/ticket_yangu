e73f2f55460fc45bd5ff2b5c4564b3d4afcc6ad7
OBrian16 <briwell16@gmail.com>
2019-07-07 11:27:48 +0300

    Add event page

---
+++ frontend/src/pages/events.js
@@ -0,0 +1,156 @@
+import React from 'react'
+import { Card, Icon, Image, CardGroup, Button, Divider, Segment } from 'semantic-ui-react'
+// import { Link } from 'react-router-dom';
+
+
+const Events = () => (
+    <Card.Group itemsPerRow={4}>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/dfd.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Design For Designers</Card.Header>
+                <Card.Meta><b><code>FRI 26TH JULY</code></b></Card.Meta>
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/ols.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Old School Mondays</Card.Header>
+                <Card.Meta><b><code>MON 30TH JULY</code></b></Card.Meta>
+
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/etana.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Etana Live</Card.Header>
+                <Card.Meta><b><code>SAT 21ST AUGUST</code></b></Card.Meta>
+
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/mkms.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>My Kenya My Story</Card.Header>
+                <Card.Meta><b><code>sun 28TH AUGUST</code></b></Card.Meta>
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/dfd.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Design For Designers</Card.Header>
+                <Card.Meta><b><code>FRI 26TH JULY</code></b></Card.Meta>
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/ols.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Old School Mondays</Card.Header>
+                <Card.Meta><b><code>MON 30TH JULY</code></b></Card.Meta>
+
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/etana.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Etana Live</Card.Header>
+                <Card.Meta><b><code>SAT 21ST AUGUST</code></b></Card.Meta>
+
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/mkms.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>My Kenya My Story</Card.Header>
+                <Card.Meta><b><code>sun 28TH AUGUST</code></b></Card.Meta>
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/dfd.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Design For Designers</Card.Header>
+                <Card.Meta><b><code>FRI 26TH JULY</code></b></Card.Meta>
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/ols.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Old School Mondays</Card.Header>
+                <Card.Meta><b><code>MON 30TH JULY</code></b></Card.Meta>
+
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/etana.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Etana Live</Card.Header>
+                <Card.Meta><b><code>SAT 21ST AUGUST</code></b></Card.Meta>
+
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/mkms.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>My Kenya My Story</Card.Header>
+                <Card.Meta><b><code>sun 28TH AUGUST</code></b></Card.Meta>
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/dfd.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Design For Designers</Card.Header>
+                <Card.Meta><b><code>FRI 26TH JULY</code></b></Card.Meta>
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/ols.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Old School Mondays</Card.Header>
+                <Card.Meta><b><code>MON 30TH JULY</code></b></Card.Meta>
+
+            </Card.Content>
+        </Card>
+
+        <Card color='grey' link>
+            <Image src={require('../images/eventImages/etana.jpg')}
+                wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Etana Live</Card.Header>
+                <Card.Meta><b><code>SAT 21ST AUGUST</code></b></Card.Meta>
+
+            </Card.Content>
+        </Card>
+
+
+    </Card.Group>
+)
+
+export default Events
