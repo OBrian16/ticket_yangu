2669d301dc5eb8d328a244727699c36ef9ebbbf3
OBrian16 <briwell16@gmail.com>
2019-07-07 12:56:02 +0300

    Add search placeholder

--- frontend/src/pages/search.js
+++ frontend/src/pages/search.js
@@ -10,45 +10,46 @@ const source = _.times(5, () => ({
 }))
 
 export default class SearchExampleStandard extends Component {
-  state = initialState
+    state = initialState
 
-  handleResultSelect = (e, { result }) => this.setState({ value: result.title })
+    handleResultSelect = (e, { result }) => this.setState({ value: result.title })
 
-  handleSearchChange = (e, { value }) => {
-    this.setState({ isLoading: true, value })
+    handleSearchChange = (e, { value }) => {
+        this.setState({ isLoading: true, value })
 
-    setTimeout(() => {
-      if (this.state.value.length < 1) return this.setState(initialState)
+        setTimeout(() => {
+            if (this.state.value.length < 1) return this.setState(initialState)
 
-      const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
-      const isMatch = result => re.test(result.title)
+            const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
+            const isMatch = result => re.test(result.title)
 
-      this.setState({
-        isLoading: false,
-        results: _.filter(source, isMatch),
-      })
-    }, 300)
-  }
+            this.setState({
+                isLoading: false,
+                results: _.filter(source, isMatch),
+            })
+        }, 300)
+    }
 
-  render() {
-    const { isLoading, value, results } = this.state
+    render() {
+        const { isLoading, value, results } = this.state
 
-    return (
-      <Grid>
-        <Grid.Column >
-          <Search
-            loading={isLoading}
-            onResultSelect={this.handleResultSelect}
-            onSearchChange={_.debounce(this.handleSearchChange, 500, {
-              leading: true,
-            })}
-            results={results}
-            value={value}
-            {...this.props}
-          />
-        </Grid.Column>
+        return (
+            <Grid>
+                <Grid.Column >
+                    <Search
+                        focus placeholder='Search...'
+                        loading={isLoading}
+                        onResultSelect={this.handleResultSelect}
+                        onSearchChange={_.debounce(this.handleSearchChange, 500, {
+                            leading: true,
+                        })}
+                        results={results}
+                        value={value}
+                        {...this.props}
+                    />
+                </Grid.Column>
 
-      </Grid>
-    )
-  }
+            </Grid>
+        )
+    }
 }
