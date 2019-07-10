b9e690efceb4c07f927b81c5281dee1e255c5804
OBrian16 <briwell16@gmail.com>
2019-07-10 21:41:57 +0300

    Edit login button

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { loginUser } from "../../actions/authActions";
 import classnames from "classnames";
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
 class Login extends Component {
   constructor() {
@@ -104,18 +105,10 @@ class Login extends Component {
                 </span>
               </div>
               <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                <button
-                  style={{
-                    width: "150px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px",
-                    marginTop: "1rem"
-                  }}
-                  type="submit"
-                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                >
+
+                <Button color='teal' size='large'type='submit' >
                   Login
-                </button>
+                </Button>
               </div>
             </form>
           </div>
