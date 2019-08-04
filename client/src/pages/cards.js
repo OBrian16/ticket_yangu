ca8dc8081e64b440ee4f27fd2f7f308ba6afbfc8
OBrian16 <briwell16@gmail.com>
2019-08-04 20:04:48 +0300

    Create event rating

--- client/src/pages/cards.js
+++ client/src/pages/cards.js
@@ -1,48 +1,84 @@
 import React from 'react'
-import { Card, Image } from 'semantic-ui-react'
+import { Card, Image, Popup, Rating } from 'semantic-ui-react'
 import { Link } from 'react-router-dom';
 
 const EventCard = () => (
     <Card.Group itemsPerRow={4}>
 
-        <Card color='grey'
-            as={Link} name='EVENTS' to='ViewEvent'>
-            <Image src={require('../images/eventImages/dfd.jpg')}
-                wrapped ui={false} />
-            <Card.Content>
-                <Card.Header>Design For Designers</Card.Header>
-                <Card.Meta><b><code>FRI 26TH JULY</code></b></Card.Meta>
-            </Card.Content>
-        </Card>
+        <Popup
+            trigger={
+                <Card color='grey'
+                    as={Link} name='EVENTS' to='ViewEvent'>
+                    <Image src={require('../images/eventImages/dfd.jpg')}
+                        wrapped ui={false} />
+                    <Card.Content>
+                        <Card.Header>Design For Designers</Card.Header>
+                        <Card.Meta><b><code>FRI 26TH JULY</code></b></Card.Meta>
+                    </Card.Content>
+                </Card>
+            }
+        >
+            <Popup.Header>Event Rating</Popup.Header>
+            <Popup.Content>
+                <Rating icon='star' defaultRating={4} maxRating={5} />
+            </Popup.Content>
+        </Popup>
 
-        <Card color='grey' link>
-            <Image src={require('../images/eventImages/ols.jpg')}
-                wrapped ui={false} />
-            <Card.Content>
-                <Card.Header>Old School Mondays</Card.Header>
-                <Card.Meta color><b><code>MON 30TH JULY</code></b></Card.Meta>
+        <Popup
+            trigger={
+                <Card color='grey' link>
+                    <Image src={require('../images/eventImages/ols.jpg')}
+                        wrapped ui={false} />
+                    <Card.Content>
+                        <Card.Header>Old School Mondays</Card.Header>
+                        <Card.Meta color><b><code>MON 30TH JULY</code></b></Card.Meta>
 
-            </Card.Content>
-        </Card>
+                    </Card.Content>
+                </Card>
+            }
+        >
+            <Popup.Header>Event Rating</Popup.Header>
+            <Popup.Content>
+                <Rating icon='star' defaultRating={5} maxRating={5} />
+            </Popup.Content>
+        </Popup>
 
-        <Card color='grey' link>
-            <Image src={require('../images/eventImages/etana.jpg')}
-                wrapped ui={false} />
-            <Card.Content>
-                <Card.Header>Etana Live</Card.Header>
-                <Card.Meta><b><code>SAT 21ST AUGUST</code></b></Card.Meta>
+        <Popup
+            trigger={
+                <Card color='grey' link>
+                    <Image src={require('../images/eventImages/etana.jpg')}
+                        wrapped ui={false} />
+                    <Card.Content>
+                        <Card.Header>Etana Live</Card.Header>
+                        <Card.Meta><b><code>SAT 21ST AUGUST</code></b></Card.Meta>
 
-            </Card.Content>
-        </Card>
+                    </Card.Content>
+                </Card>
+            }
+        >
+            <Popup.Header>Event Rating</Popup.Header>
+            <Popup.Content>
+                <Rating icon='star' defaultRating={3} maxRating={5} />
+            </Popup.Content>
+        </Popup>
 
-        <Card color='grey' link>
-            <Image src={require('../images/eventImages/mkms.jpg')}
-                wrapped ui={false} />
-            <Card.Content>
-                <Card.Header>My Kenya My Story</Card.Header>
-                <Card.Meta><b><code>SUN 28TH AUGUST</code></b></Card.Meta>
-            </Card.Content>
-        </Card>
+        <Popup
+            trigger={
+                <Card color='grey' link>
+                    <Image src={require('../images/eventImages/mkms.jpg')}
+                        wrapped ui={false} />
+                    <Card.Content>
+                        <Card.Header>My Kenya My Story</Card.Header>
+                        <Card.Meta><b><code>SUN 28TH AUGUST</code></b></Card.Meta>
+                    </Card.Content>
+                </Card>
+            }
+        >
+            <Popup.Header>Event Rating</Popup.Header>
+            <Popup.Content>
+                <Rating icon='star' defaultRating={2} maxRating={5} />
+            </Popup.Content>
+        </Popup>
 
     </Card.Group>
 )
